1639942a54cdf829a62bd29f018cd9c92a16e2b3
Naomi <hebert.naomi@courrier.uqam.ca>
2024-11-13 23:39:55 -0500

    Added Markview plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,4 +6,23 @@ return {
   {
     'mbbill/undotree',
   },
+  -- install with yarn or npm
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+    init = function()
+      require('markview').setup {
+        hybrid_modes = { 'n' },
+        links = {
+          enable = true,
+        },
+      }
+    end,
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
 }
